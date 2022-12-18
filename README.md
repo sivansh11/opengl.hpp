1890007e90272a536662516b14bc616c890ce688
sivansh11 <sivanshgupta2002@gmail.com>
2022-12-19 02:51:52 +0530

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # opengl.hpp
 this is inspired by vulkan.hpp 
+
+NOTE: this opengl bindings currently only impliments opengl dsa
+
+At the moment I have only implimented buffers (not even vertex attributes),
+
+All dsa buffer operations/functions are now a part of class Buffer
+
+this
+```
+GLuint id;
+glCreateBuffer(1, id);
+GLbitfield flags = ...;
+glNamedBufferData(id, sizeof(Data), &data, flags);
+```
+
+becomes
+```
+gl::Buffer buffer = gl::Buffer::createBuffer();
+gl::BufferUsage flags = ...;
+buffer.data(sizeof(Data), &data, flags);
+```
+
+opengl.hpp not only reduces loc (tho marginally), it converts runtime errors to compile time errors. It does this by forcing the user to use the correct flags. For example
+
+glNamedBufferStorage may take in combination of many opengl enums, this may be error prone, </br>opengl.hpp combines all similar flags/enums together under a common type
+for example all buffer storage flags are grouped under BufferStorage
+
+all flags follow a common transformation
+for example 
+GL_STATIC_DRAW is a buffer usage flag
+this gets converted to gl::BufferUsage::eStaticDraw
+
--- src/types.hpp
+++ src/types.hpp
@@ -51,6 +51,57 @@ public:
     }
 };
 
+class BufferUsage : virtual public BaseFlag<GLenum> {
+public: 
+    BufferUsage() = default;
+    BufferUsage(GLbitfield flag) : BaseFlag(flag) {}
+    operator GLenum() {
+        return flags;
+    }
+    static constexpr GLenum eStreamDraw = GL_STREAM_DRAW;
+    static constexpr GLenum eStreamCopy = GL_STREAM_COPY;
+    static constexpr GLenum eStreamRead = GL_STREAM_READ;
+
+    static constexpr GLenum eStaticDraw = GL_STATIC_DRAW;
+    static constexpr GLenum eStaticCopy = GL_STATIC_COPY;
+    static constexpr GLenum eStaticRead = GL_STATIC_READ;
+
+    static constexpr GLenum eDynamicDraw = GL_DYNAMIC_DRAW;
+    static constexpr GLenum eDynamicCopy = GL_DYNAMIC_COPY;
+    static constexpr GLenum eDynamicRead = GL_DYNAMIC_READ;
+};
+
+class BufferMap : virtual public BaseFlag<GLbitfield> {
+public:
+    BufferMap() = default;
+    BufferMap(GLbitfield flag) : BaseFlag(flag) {}
+    operator GLbitfield() {
+        return flags;
+    }
+    static constexpr GLbitfield eRead = GL_MAP_READ_BIT;
+    static constexpr GLbitfield eWrite = GL_MAP_WRITE_BIT;
+    static constexpr GLbitfield ePersistent = GL_MAP_PERSISTENT_BIT;
+    static constexpr GLbitfield eCoherent = GL_MAP_COHERENT_BIT;
+    static constexpr GLbitfield eInvalidateRange = GL_MAP_INVALIDATE_RANGE_BIT;
+    static constexpr GLbitfield eInvalidate = GL_MAP_INVALIDATE_BUFFER_BIT;
+    static constexpr GLbitfield eFlushExplicit = GL_MAP_FLUSH_EXPLICIT_BIT;
+    static constexpr GLbitfield eUnsynchronized = GL_MAP_UNSYNCHRONIZED_BIT;
+
+};
+
+class BufferStorage : virtual public BufferMap {
+public:
+    BufferStorage() = default;
+    BufferStorage(GLbitfield flag) : BaseFlag(flag) {}
+    operator GLbitfield() {
+        return flags;
+    }
+    static constexpr GLbitfield eDynamic = GL_DYNAMIC_STORAGE_BIT;
+    static constexpr GLbitfield eClient = GL_CLIENT_STORAGE_BIT;
+    static constexpr GLbitfield eNone = GL_NONE;
+};
+
+
 // enum class TextureType : GLint {
 //     e1D = GL_TEXTURE_1D,
 //     e2D = GL_TEXTURE_2D,
@@ -114,56 +165,6 @@ public:
 //     eLinear = GL_LINEAR,
 //     eNone = GL_NONE,
 // };
-
-class BufferUsage : virtual public BaseFlag<GLenum> {
-public: 
-    BufferUsage() = default;
-    BufferUsage(GLbitfield flag) : BaseFlag(flag) {}
-    operator GLenum() {
-        return flags;
-    }
-    static constexpr GLenum eStreamDraw = GL_STREAM_DRAW;
-    static constexpr GLenum eStreamCopy = GL_STREAM_COPY;
-    static constexpr GLenum eStreamRead = GL_STREAM_READ;
-
-    static constexpr GLenum eStaticDraw = GL_STATIC_DRAW;
-    static constexpr GLenum eStaticCopy = GL_STATIC_COPY;
-    static constexpr GLenum eStaticRead = GL_STATIC_READ;
-
-    static constexpr GLenum eDynamicDraw = GL_DYNAMIC_DRAW;
-    static constexpr GLenum eDynamicCopy = GL_DYNAMIC_COPY;
-    static constexpr GLenum eDynamicRead = GL_DYNAMIC_READ;
-};
-
-class BufferMap : virtual public BaseFlag<GLbitfield> {
-public:
-    BufferMap() = default;
-    BufferMap(GLbitfield flag) : BaseFlag(flag) {}
-    operator GLbitfield() {
-        return flags;
-    }
-    static constexpr GLbitfield eRead = GL_MAP_READ_BIT;
-    static constexpr GLbitfield eWrite = GL_MAP_WRITE_BIT;
-    static constexpr GLbitfield ePersistent = GL_MAP_PERSISTENT_BIT;
-    static constexpr GLbitfield eCoherent = GL_MAP_COHERENT_BIT;
-    static constexpr GLbitfield eInvalidateRange = GL_MAP_INVALIDATE_RANGE_BIT;
-    static constexpr GLbitfield eInvalidate = GL_MAP_INVALIDATE_BUFFER_BIT;
-    static constexpr GLbitfield eFlushExplicit = GL_MAP_FLUSH_EXPLICIT_BIT;
-    static constexpr GLbitfield eUnsynchronized = GL_MAP_UNSYNCHRONIZED_BIT;
-
-};
-
-class BufferStorage : virtual public BufferMap {
-public:
-    BufferStorage() = default;
-    BufferStorage(GLbitfield flag) : BaseFlag(flag) {}
-    operator GLbitfield() {
-        return flags;
-    }
-    static constexpr GLbitfield eDynamic = GL_DYNAMIC_STORAGE_BIT;
-    static constexpr GLbitfield eClient = GL_CLIENT_STORAGE_BIT;
-    static constexpr GLbitfield eNone = GL_NONE;
-};
 
 } // namespace gl
 
